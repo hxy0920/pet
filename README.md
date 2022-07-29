5aaf5e6f8151af5eaba3d245e7ba4e4309a3c70f
huxinyue <huxinyue@idcos.com>
2022-07-29 15:51:21 +0800

    :construction: 引入响应式布局和icon

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 
 ### 日志
 
-#### 2022.7,28
+#### 2022.7.29
+
++ 引入Icon和响应式布局
+
+#### 2022.7.28
 
 + app.use(router) 需要放在app.mount('#app')前面 不然加载时router-view、router-link等未被渲染 会报错vue3: Failed to resolve component: router-view
 + 记得给 script 标签添加一个 setup 属性 使整个 script 变成 setup 函数
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,44 +1,61 @@
 <template lang="">
   <el-row class="login">
-    <el-col :span="16" class="left">
+    <el-col :lg="16" :md="12" class="left">
       <div class="text-left text-light-50 font-bold">
         <div class="border-l text-5xl mb-4">欢迎光临</div>
-        <div class="text-gray-200 text-sm">来到我的宠物知识网来到我的宠物知识网来到我的宠物知识网</div>
+        <div class="text-gray-200 text-sm">
+          来到我的宠物知识网来到我的宠物知识网来到我的宠物知识网
+        </div>
       </div>
     </el-col>
-    <el-col :span="8" class="right">
+    <el-col :lg="8" :md="12" class="right">
       <h2 class="font-bold text-3xl text-gray-700">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
+      <div
+        class="flex items-center justify-center my-5 text-gray-300 space-x-2"
+      >
         <span class="h-[1px] w-16 bg-gray-200"></span>
         <span class="">账号密码登录</span>
         <span class="h-[1px] w-16 bg-gray-200"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
-        <el-form-item >
-          <el-input v-model="form.username" placeholder="请输入账号"/>
-        </el-form-item>
-        <el-form-item >
-          <el-input v-model="form.password" placeholder="请输入密码"/>
+        <el-form-item>
+          <el-input v-model="form.username" placeholder="请输入账号">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit" color="#43946c" class="w-[250px] rounded-3xl">登 录</el-button>
+          <el-input v-model="form.password" placeholder="请输入密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="onSubmit"
+            color="#43946c"
+            class="w-[250px] rounded-3xl"
+            >登 录</el-button
+          >
         </el-form-item>
       </el-form>
     </el-col>
   </el-row>
 </template>
 <script lang="ts" setup>
-import { reactive } from 'vue'
-
+import { reactive } from "vue";
 // do not use same name with ref
 const form = reactive({
-  username: '',
-  password:'',
-})
+  username: "",
+  password: "",
+});
 
 const onSubmit = () => {
-  console.log('submit!')
-}
+  console.log("submit!");
+};
 </script>
 <style>
 .login {
